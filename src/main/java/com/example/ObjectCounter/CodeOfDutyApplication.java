9b21abf4eba6fbc6a7b6c67a28c06987061bb232
Harsha Salim <harsha.salim@thoughtworks.com>
2023-07-14 11:06:08 +0530

    Change rootProject name to CodeOfDuty, and update application name to CodeOfDutyApplication

--- src/main/java/com/example/ObjectCounter/CodeOfDutyApplication.java
+++ src/main/java/com/example/ObjectCounter/CodeOfDutyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ObjectCounterApplication {
+public class CodeOfDutyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ObjectCounterApplication.class, args);
+		SpringApplication.run(CodeOfDutyApplication.class, args);
 	}
 
 }
